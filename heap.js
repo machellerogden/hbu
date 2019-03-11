3025394d16c8f85f9971aff2a4cf75ed8b80b6ae
Mac Heller-Ogden <mac@machellerogden.com>
2019-03-11 12:06:51 -0500

    collecting some notes

---
+++ heap.js
@@ -0,0 +1,4 @@
+// reading:
+// * https://github.com/nodejs/diagnostics/issues/148
+// * https://nodejs.org/en/docs/guides/simple-profiling/
+// * https://firebearstudio.com/blog/node-js-prof-profiling-node-js-applications.html
