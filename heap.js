d9a09d73323284e0963a4b3655c8f5f4f3dd04f1
Mac Heller-Ogden <mac@machellerogden.com>
2019-03-11 12:54:56 -0500

    more reference

--- heap.js
+++ heap.js
@@ -5,3 +5,4 @@
 // * https://gist.github.com/listochkin/10973974
 // * https://github.com/thlorenz/v8-perf
 // * https://link.medium.com/LQPyYsizYU
+// * http://jayconrod.com/posts/55/a-tour-of-v8-garbage-collection
