d614608b482a3f6ad6db8ce049aba8943a7bc2cd
Mac Heller-Ogden <mac@machellerogden.com>
2019-03-11 12:46:24 -0500

    another link

--- heap.js
+++ heap.js
@@ -2,3 +2,4 @@
 // * https://github.com/nodejs/diagnostics/issues/148
 // * https://nodejs.org/en/docs/guides/simple-profiling/
 // * https://firebearstudio.com/blog/node-js-prof-profiling-node-js-applications.html
+// * https://gist.github.com/listochkin/10973974
