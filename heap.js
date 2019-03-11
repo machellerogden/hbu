ed226c0dda0cb53f511a60004416ba3ce9390813
Mac Heller-Ogden <mac@machellerogden.com>
2019-03-11 12:56:53 -0500

    most important reference

--- heap.js
+++ heap.js
@@ -1,4 +1,5 @@
 // reading:
+// * https://nodejs.org/api/v8.html
 // * https://github.com/nodejs/diagnostics/issues/148
 // * https://nodejs.org/en/docs/guides/simple-profiling/
 // * https://firebearstudio.com/blog/node-js-prof-profiling-node-js-applications.html
