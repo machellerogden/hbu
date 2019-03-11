baa0769398afb898680d5cb82a72b83541ba6cad
Mac Heller-Ogden <mac@machellerogden.com>
2019-03-11 13:00:47 -0500

    gc-stats

--- heap.js
+++ heap.js
@@ -1,5 +1,6 @@
 // reading:
 // * https://nodejs.org/api/v8.html
+// * https://www.npmjs.com/package/gc-stats
 // * https://github.com/nodejs/diagnostics/issues/148
 // * https://nodejs.org/en/docs/guides/simple-profiling/
 // * https://firebearstudio.com/blog/node-js-prof-profiling-node-js-applications.html
