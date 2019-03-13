7e7ba6ad282c4ca726b4c93bec32cab198854277
Mac Heller-Ogden <mac@machellerogden.com>
2019-03-13 11:13:55 -0500

    starting to put together fns

--- example/example-test.js
+++ example/example-test.js
@@ -1,13 +1,15 @@
-const data = {
-    a: "a",
-    b: "b",
-    c: "c"
-};
+global.hbu_run(() => {
+    const data = {
+        a: "a",
+        b: "b",
+        c: "c"
+    };
 
-const leak = [];
+    const leak = [];
 
-let i = 0;
-while (i < process.env.HBU_TIMES) {
-    leak.push(JSON.parse(JSON.stringify(data)));
-    i++;
-}
+    let i = 0;
+    while (i < process.env.HBU_TIMES) {
+        leak.push(JSON.parse(JSON.stringify(data)));
+        i++;
+    }
+});
--- instrument.js
+++ instrument.js
@@ -15,38 +15,47 @@ const obs = new PerformanceObserver((items) => {
 obs.observe({ entryTypes: ['measure'] });
 
 process.on('exit', () => {
-    performance.mark(`${label}_end`);
-    process.send({
-        type: 'memory_usage',
-        data: process.memoryUsage()
-    });
-    performance.measure(label, `${label}_start`, `${label}_end`);
-    obs.disconnect();
     process.send({
         type: 'done'
     });
 });
 
+global.hbu_run = (fn) => {
 
-if (process.env.HBU_GC_STATS) {
-    let skip_gc_send = true;
-    require('gc-stats')().on('stats', stats => {
-        if (skip_gc_send) {
-            skip_gc_send = false;
-            return;
-        }
-        process.send({
-            type: 'gc_event',
-            data: stats
+    global.gc();
+
+    if (process.env.HBU_GC_STATS) {
+        let skip_gc_send = true;
+        require('gc-stats')().on('stats', stats => {
+            if (skip_gc_send) {
+                skip_gc_send = false;
+                return;
+            }
+            process.send({
+                type: 'gc_event',
+                data: stats
+            });
         });
+        global.gc(); // one more gc - "clean room" approach
+    }
+
+    process.send({
+        type: 'memory_usage',
+        data: process.memoryUsage()
     });
-}
 
-global.gc();
+    performance.mark(`${label}_start`);
 
-process.send({
-    type: 'memory_usage',
-    data: process.memoryUsage()
-});
+    fn();
 
-performance.mark(`${label}_start`);
+    performance.mark(`${label}_end`);
+
+    process.send({
+        type: 'memory_usage',
+        data: process.memoryUsage()
+    });
+
+    performance.measure(label, `${label}_start`, `${label}_end`);
+
+    obs.disconnect();
+};
